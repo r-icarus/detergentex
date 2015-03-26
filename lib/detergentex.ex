e4f98e1d98a6384652b64675378cd78b722e390f
Ricardo Echavarria <rgechavarria@gmail.com>
2015-03-26 12:23:27 -0700

    Unnecessary assignation

--- lib/detergentex.ex
+++ lib/detergentex.ex
@@ -11,7 +11,6 @@ defmodule Detergentex do
   end
 
   def call(wsdl_url, method, params) do
-    response = Detergentex.Client.call_service(wsdl_url, method, params)
-    response
+    Detergentex.Client.call_service(wsdl_url, method, params)    
   end
 end
